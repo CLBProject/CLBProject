cafad6bf9ca51bef405b694bf7f398ec268db840
Carlos Nobre <ext.carlos.nobre@ingg.com>
2018-01-11 11:57:47 +0000

    Update Script for CSV file

--- src/main/script/FTP.py
+++ src/main/script/FTP.py
@@ -1,7 +1,10 @@
 import glob
 import os
 import sys
+import csv
 from ftplib import FTP
+
+import csv
 
 # Ligar ao FTP e à pasta processed
 ftp = FTP('ftp.dexcell.com')
@@ -15,20 +18,5 @@ data = []
 ftp.retrlines("LIST", (data.append))
 words = data[-1].split(None, 8)
 filename = words[-1].lstrip()
-print(filename)
-
-
-print ('Ficheiro mais recente: ', filename)
-
-
-# #copiar o ficheiro para o local que se quer
-local_filename = os.path.join(r"D:\Documentos\CBL\Teste", filename)
-lf = open(local_filename, "wb")
-try:
-    ftp.retrbinary("RETR " + filename, lf.write, 8*1024)
-    print(f'Ficheiro copiado com sucesso')
-except:
-    print("Algo errado não está certo")
-lf.close
 
 ftp.quit()
