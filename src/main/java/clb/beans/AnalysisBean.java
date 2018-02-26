ad856afa93683601eef7fcdff3a599a7bd68daa5
Carlos Nobre <ext.carlos.nobre@ingg.com>
2018-02-26 18:45:07 +0000

    Designing Dashboard UI

--- src/main/java/clb/beans/AnalysisBean.java
+++ src/main/java/clb/beans/AnalysisBean.java
@@ -3,8 +3,13 @@ package clb.beans;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import clb.beans.enums.AnalysisTypes;
+import clb.beans.enums.ScaleGraphic;
+import clb.beans.pojos.AnalysisBarPojo;
 
 @ViewScoped
 @ManagedBean
@@ -13,6 +18,21 @@ public class AnalysisBean implements Serializable{
     private static final long serialVersionUID = 1L;
 
     private Date analysisDate;
+    private AnalysisBarPojo analysisBarDayPojo;
+    
+    private AnalysisTypes analysisType;
+    private AnalysisTypes[] analysisTypes;
+    
+    private ScaleGraphic scaleGraphic;
+    private ScaleGraphic[] scalesGraphic;
+    
+    @PostConstruct
+    public void init() {
+        analysisDate = new Date();
+        analysisBarDayPojo = new AnalysisBarPojo();
+        analysisTypes = AnalysisTypes.values();
+        scalesGraphic = ScaleGraphic.values();
+    }
 
     public Date getAnalysisDate() {
         return analysisDate;
@@ -20,6 +40,46 @@ public class AnalysisBean implements Serializable{
 
     public void setAnalysisDate( Date analysisDate ) {
         this.analysisDate = analysisDate;
+    }
+
+    public AnalysisBarPojo getAnalysisBarDayPojo() {
+        return analysisBarDayPojo;
+    }
+
+    public void setAnalysisBarDayPojo( AnalysisBarPojo analysisBarDayPojo ) {
+        this.analysisBarDayPojo = analysisBarDayPojo;
+    }
+
+    public AnalysisTypes getAnalysisType() {
+        return analysisType;
+    }
+
+    public void setAnalysisType( AnalysisTypes analysisType ) {
+        this.analysisType = analysisType;
+    }
+
+    public AnalysisTypes[] getAnalysisTypes() {
+        return analysisTypes;
+    }
+
+    public void setAnalysisTypes( AnalysisTypes[] analysisTypes ) {
+        this.analysisTypes = analysisTypes;
+    }
+
+    public ScaleGraphic[] getScalesGraphic() {
+        return scalesGraphic;
+    }
+
+    public void setScalesGraphic( ScaleGraphic[] scalesGraphic ) {
+        this.scalesGraphic = scalesGraphic;
+    }
+
+    public ScaleGraphic getScaleGraphic() {
+        return scaleGraphic;
+    }
+
+    public void setScaleGraphic( ScaleGraphic scaleGraphic ) {
+        this.scaleGraphic = scaleGraphic;
     }
     
     
