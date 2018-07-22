69d9b638761b4bb9b8868b2d6b1a58c45cd8d63b
CLB <CLB.Projecto@gmail.com>
2018-07-22 02:19:45 +0100

    Adding Tests and sonar fix

--- src/test/java/business/AnalyzerDataServiceTest.java
+++ src/test/java/business/AnalyzerDataServiceTest.java
@@ -4,36 +4,39 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.task.TaskExecutor;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import clb.business.AnalyzerDataService;
+import clb.business.AnalyzerDataServiceImpl;
 import clb.database.ClbDao;
+import clb.database.ClbDaoImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/applicationContext.xml"})
+@ContextConfiguration(classes= {
+		AnalyzerDataServiceImpl.class,
+		ClbDaoImpl.class
+})
 public class AnalyzerDataServiceTest {
 
 	@Autowired
 	private AnalyzerDataService analyzerDataService;
 
-	@Mock
-	private TaskExecutor taskExecutor;
+	@Autowired
+	TaskExecutor taskExecutor;
 	
 	@Mock
 	private ClbDao clbDao;
 	
-	@Mock
-	private MongoTemplate mongoTemplate;
-
 	@Before
 	public void setUp() {
+		Mockito.mock(TaskExecutor.class);
 		MockitoAnnotations.initMocks(this);
 	}
 
