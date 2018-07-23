854c1e712d6ed146ad6300a4ca5b61837fc34e99
Carlos Nobre <ext.carlos.nobre@ingg.com>
2018-07-23 12:14:48 +0100

    Test Fixing Spring

--- src/test/java/business/AnalyzerDataServiceTest.java
+++ src/test/java/business/AnalyzerDataServiceTest.java
@@ -1,20 +1,24 @@
 package business;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import business.configuration.ConfigurationBusiness;
 import clb.business.AnalyzerDataService;
 import clb.database.ClbDao;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations= {"classpath:applicationContext.xml"})
+@ContextConfiguration(classes= {
+		ConfigurationBusiness.class
+})
 public class AnalyzerDataServiceTest {
 
 	@Autowired
@@ -22,9 +26,6 @@ public class AnalyzerDataServiceTest {
 
 	@Autowired
 	private ClbDao clbDao;
-
-	@Mock
-	MongoTemplate mongoTemplate;
 
 	@Before
 	public void setup() {
@@ -34,6 +35,8 @@ public class AnalyzerDataServiceTest {
 	public void testGetYearsAvailable() {
 
 		String[] years = {"2016","2017","2018"};
+		
+		when(clbDao.getYearsAvailable()).thenReturn(years);
 
 		String[] yearsReturned = analyzerDataService.getYearsAvailable();
 
