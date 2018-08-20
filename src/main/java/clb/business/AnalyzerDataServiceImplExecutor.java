4fad89609fb75f72fe0e5de7438a7a99856ea582
CLB <CLB.Projecto@gmail.com>
2018-08-20 03:33:07 +0100

    Docker Split and Fix.. Script Python Integration

--- src/main/java/clb/business/AnalyzerDataServiceImplExecutor.java
+++ src/main/java/clb/business/AnalyzerDataServiceImplExecutor.java
@@ -3,13 +3,16 @@ package clb.business;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import org.primefaces.json.JSONException;
 import org.primefaces.json.JSONObject;
 
 import clb.business.exceptions.IlegalCommandAppException;
+import clb.business.objects.UsersystemObject;
 import clb.database.ClbDao;
 
 public class AnalyzerDataServiceImplExecutor implements Runnable{
@@ -29,33 +32,34 @@ public class AnalyzerDataServiceImplExecutor implements Runnable{
 				try(Scanner in = new Scanner(clientSocket.getInputStream())){
 
 					boolean exit = false;
-					
+
 					while(clientSocket.isConnected() && !exit) {
 						String command = in.nextLine();
 
 						//Send Users
 						if(command.equals("*getUsersInfo*")) {
-							//							List<JSONObject> jsonToSend = clbDao.getAllUsers().stream()
-							//									.map(UsersystemObject::toJson)
-							//									.collect(Collectors.toList());
-							//
-							//							for(JSONObject json: jsonToSend) {
-							//								clientSocket.getOutputStream().write(json.toString().getBytes());
-							//							}
-							char s1 = '"';
+							List<JSONObject> jsonToSend = clbDao.getAllUsers().stream()
+									.map(UsersystemObject::toJson)
+									.collect(Collectors.toList());
 
-							clientSocket.getOutputStream().write(("{"+s1+"userftp"+s1+":"+s1+"greenworld@ventosdepoupanca.com"+s1+", "
-									+ ""+s1+"passwordftp"+s1+":"+s1+"l#_YqMoJe%coJUbF"+s1+"}\n").getBytes());
-							clientSocket.getOutputStream().flush();
-							clientSocket.getOutputStream().write(("{"+s1+"userftp"+s1+":"+s1+"greenworld@ventosdepoupanca.com"+s1+", "
-									+ ""+s1+"passwordftp"+s1+":"+s1+"l#_YqMoJe%coJUbF"+s1+"}\n").getBytes());
-							clientSocket.getOutputStream().flush();
+//							for(JSONObject json: jsonToSend) {
+//								clientSocket.getOutputStream().write(json.toString().getBytes());
+//								clientSocket.getOutputStream().flush();
+//							}	
+							
+							JSONObject usersSend = new JSONObject();
+							
+							usersSend.append("userftp", "greenworld@ventosdepoupanca.com");
+							usersSend.append("passwordftp", "l#_YqMoJe%%coJUbF");
+							clientSocket.getOutputStream().write(usersSend.toString().getBytes());
+//							clientSocket.getOutputStream().flush();
+							
 							clientSocket.getOutputStream().write("*end*".getBytes());
-							clientSocket.getOutputStream().flush();
 						}
 						//Persist Data Objects
 						else if(command.equals("*persistDataObject*")){
 							JSONObject jsonObj = new JSONObject(in.nextLine());
+							System.out.println(jsonObj.toString());
 							//clbDao.saveAnalyzerRegistry(new AnalyzerRegistryObject(jsonObj));
 						}
 						else if(command.equals("*exit*")){
