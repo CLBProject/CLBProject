5aa8f468f8d9ca476bf223ec62dd87782c893d6f
CLB <CLB.Projecto@gmail.com>
2018-08-27 03:52:25 +0100

    Script Improvment

--- src/main/script/schoolsFtp/clbDataScript.py
+++ src/main/script/schoolsFtp/clbDataScript.py
@@ -6,6 +6,7 @@ from ftplib import FTP
 import configparser
 import socket
 import json
+import threading
 
 FTP_HOST = 'ftp.mobinteg.org'
 SERVER_HOST = "localhost"
@@ -62,65 +63,62 @@ class AnalyzerRegistry:
     def toJson(self):
         return self.kwl1
 
-def processData(more_data, filename):
-    dataProcessed = more_data.decode("utf-8")
+def processData(line, filename):
     
+    c = line.split(";")
+        
+    recortType = c[0]
+        
+    if recortType != 'AC' or len(c) < 39:
+        return
+        
+        
+    productType = c[1]
+    itemSn = c[2]
+    itemLabel = c[3]
+    comPort = c[4]
+    modBusId = c[5]
+    date = c[6]
+    time = c[7]
+    kwh = c[8]
+    kWhNeg = c[9]
+    vlnsys = c[10]
+    vl1n = c[11]
+    vl2n = c[12]
+    vl3n = c[13]
+    vllsys = c[14]
+    vl1l2 = c[15]
+    vl2l3 = c[16]
+    vl3l1 = c[17]
+    al1 = c[18]
+    al2 = c[19]
+    al3 = c[20]
+    kwsys = c[21]
+    kwl1 = c[22]
+    kwl2 = c[23]  
+    kwl3 = c[24]
+    kvarsys = c[25]
+    kvarl1 = c[26]
+    kvarl2 = c[27]
+    kvarl3 = c[28]
+    kvasys = c[29]
+    kval1 = c[30]
+    kval2 = c[31]
+    kval3 = c[32]
+    pfsys = c[33]
+    pfl1 = c[34]
+    pfl2 = c[35]
+    pfl3 = c[36]
+    phaseSequence = c[37]
+    hZ = c[38]
+        
+    analyzerReg = AnalyzerRegistry(recortType,productType, itemSn, itemLabel, comPort, modBusId,
+    date, time, kwh, kWhNeg, vlnsys, vl1n, vl2n, vl3n, vllsys, vl1l2, vl2l3, vl3l1,
+    al1, al2, al3, kwsys, kwl1, kwl2, kwl3, kvarsys, kvarl1, kvarl2, kvarl3, kvasys, 
+    kval1, kval2, kval3, pfsys, pfl1, pfl2, pfl3, phaseSequence, hZ, filename)
     
-    for eachlineCommas in dataProcessed.splitlines():
-        c = eachlineCommas.split(";")
-        
-        recortType = c[0]
-        
-        if recortType != 'AC' or len(c) < 39:
-            continue
-        
-        
-        productType = c[1]
-        itemSn = c[2]
-        itemLabel = c[3]
-        comPort = c[4]
-        modBusId = c[5]
-        date = c[6]
-        time = c[7]
-        kwh = c[8]
-        kWhNeg = c[9]
-        vlnsys = c[10]
-        vl1n = c[11]
-        vl2n = c[12]
-        vl3n = c[13]
-        vllsys = c[14]
-        vl1l2 = c[15]
-        vl2l3 = c[16]
-        vl3l1 = c[17]
-        al1 = c[18]
-        al2 = c[19]
-        al3 = c[20]
-        kwsys = c[21]
-        kwl1 = c[22]
-        kwl2 = c[23]  
-        kwl3 = c[24]
-        kvarsys = c[25]
-        kvarl1 = c[26]
-        kvarl2 = c[27]
-        kvarl3 = c[28]
-        kvasys = c[29]
-        kval1 = c[30]
-        kval2 = c[31]
-        kval3 = c[32]
-        pfsys = c[33]
-        pfl1 = c[34]
-        pfl2 = c[35]
-        pfl3 = c[36]
-        phaseSequence = c[37]
-        hZ = c[38]
-        
-        analyzerReg = AnalyzerRegistry(recortType,productType, itemSn, itemLabel, comPort, modBusId,
-        date, time, kwh, kWhNeg, vlnsys, vl1n, vl2n, vl3n, vllsys, vl1l2, vl2l3, vl3l1,
-        al1, al2, al3, kwsys, kwl1, kwl2, kwl3, kvarsys, kvarl1, kvarl2, kvarl3, kvasys, 
-        kval1, kval2, kval3, pfsys, pfl1, pfl2, pfl3, phaseSequence, hZ, filename)
-        
-        sock.send(bytes('*persistDataObject*\n', 'utf-8'))
-        sock.send(bytes(json.dumps(analyzerReg.__dict__)+"\n", 'utf-8'))
+    sock.send(bytes('*persistDataObject*\n', 'utf-8'))
+    sock.send(bytes(json.dumps(analyzerReg.__dict__)+"\n", 'utf-8'))
 
 def processUserFtp(userftp,passwordftp):
 
@@ -145,18 +143,17 @@ def processUserFtp(userftp,passwordftp):
         for file in data:
             #Ignore the first two files
             if index > 1:
-                filename = file.split(None, 8)[-1].lstrip()
-                #print ('Ficheiro mais recente: ', filename)
-
-                ftp.retrbinary('RETR '+filename,  lambda block: processData(block, currentDir), 1024)
-                
-            index = index + 1
+                    filename = file.split(None, 8)[-1].lstrip()
+                    #print ('Ficheiro mais recente: ', filename)
+                    ftp.retrlines('RETR ' + filename, lambda line: processData(line, currentDir))
+            
+            index = index +1
                 
         ftp.cwd("..")
 
     ftp.quit()
         
-    sock.send(bytes("*exit*\n", 'utf-8'))
+    sock.send(bytes("*exitPersistData*\n", 'utf-8'))
     return;
 
 def recv_basic():
